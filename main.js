efcaa5e3800b517a5ccdfc26c638a31fc7ec1d3f
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-19 14:50:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const distanceEl = document.querySelector(".distance")
+const ageEl = document.querySelector(".age")
+//console.log(distanceEl, ageEl);
+
+function userDistance() {
+    const userQuestion = Number(prompt("Chose how many km do you want"))
+    console.log(userQuestion);
+    return
+}
+
+userDistance()
